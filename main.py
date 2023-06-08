06d5042ac1d15483375d40cc76b5163eb32c876e
Rodrigo Marques <132007834+Rodrigomarques02@users.noreply.github.com>
2023-06-08 16:23:29 -0300

    Update main.py commit 8
    Nós criamos uma classe chamada Pesquisa que possui os métodos coletar_respostas() e gravar_respostas_csv(). O método coletar_respostas() realiza a coleta das respostas do usuário, armazenando-as na lista self.respostas do objeto Pesquisa. O método gravar_respostas_csv() formata as respostas como uma linha CSV e as grava em um arquivo, seguindo as boas práticas mencionadas anteriormente.
    
    Em seguida, criamos um objeto pesquisa da classe Pesquisa com as perguntas desejadas e chamamos os métodos coletar_respostas() e gravar_respostas_csv() para realizar a coleta e gravação das respostas.
    
    Essa abordagem orientada a objetos permite uma estrutura mais modular e organizada do código, facilitando a reutilização e manutenção futura do projeto.

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
 # main.py
-# Gravação das respostas em um arquivo CSV (sem usar bibliotecas)
+# Gravação das respostas em um arquivo CSV usando classes (sem usar bibliotecas)
 
+class Pesquisa:
+    def __init__(self, perguntas):
+        self.perguntas = perguntas
+        self.respostas = []
+
+    def coletar_respostas(self):
+        for pergunta in self.perguntas:
+            resposta = input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei): ")
+            self.respostas.append(resposta)
+
+    def gravar_respostas_csv(self):
+        # Formatação das respostas como uma linha CSV
+        linha_csv = ','.join(self.respostas) + '\n'
+
+        # Gravação das respostas em um arquivo CSV
+        nome_arquivo = "respostas.csv"
+
+        # Verificar se o arquivo já existe
+        arquivo_existe = False
+        try:
+            with open(nome_arquivo, "r"):
+                arquivo_existe = True
+        except FileNotFoundError:
+            arquivo_existe = False
+
+        # Escrever o cabeçalho se o arquivo não existir
+        if not arquivo_existe:
+            cabecalho = ','.join(self.perguntas) + '\n'
+            with open(nome_arquivo, "w") as arquivo_csv:
+                arquivo_csv.write(cabecalho)
+
+        # Adicionar as respostas
+        with open(nome_arquivo, "a") as arquivo_csv:
+            arquivo_csv.write(linha_csv)
+
+        print("Respostas gravadas com sucesso no arquivo CSV.")
+
+# Criação do objeto de pesquisa
 perguntas = [
     "Você gosta de programar?",
     "Você está estudando Python?",
     "Você já trabalhou com análise de dados?",
     "Você se sente confortável com estatísticas?"
 ]
+pesquisa = Pesquisa(perguntas)
 
-respostas = [input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei): ") for pergunta in perguntas]
-
-# Formatação das respostas como uma linha CSV
-linha_csv = ','.join(respostas) + '\n'
+# Coleta das respostas
+pesquisa.coletar_respostas()
 
 # Gravação das respostas em um arquivo CSV
-nome_arquivo = "respostas.csv"
-
-# Verificar se o arquivo já existe
-arquivo_existe = False
-try:
-    with open(nome_arquivo, "r"):
-        arquivo_existe = True
-except FileNotFoundError:
-    arquivo_existe = False
-
-# Escrever o cabeçalho se o arquivo não existir
-if not arquivo_existe:
-    with open(nome_arquivo, "w") as arquivo_csv:
-        cabecalho = ','.join(perguntas) + '\n'
-        arquivo_csv.write(cabecalho)
-
-# Adicionar as respostas
-with open(nome_arquivo, "a") as arquivo_csv:
-    arquivo_csv.write(linha_csv)
-
-print("Respostas gravadas com sucesso no arquivo CSV.")
+pesquisa.gravar_respostas_csv()
 
 # Documentação
 """
 Esse programa realiza uma pesquisa com 4 perguntas e grava as respostas em um arquivo CSV.
 As perguntas são exibidas sequencialmente, e o usuário deve responder com 1 para Sim, 2 para Não, ou 3 para Não sei.
 As respostas são gravadas em um arquivo CSV chamado 'respostas.csv'.
+"""
