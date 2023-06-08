16f0ce8c74871c040e9320d6f448563bc27bf24f
Rodrigo Marques <132007834+Rodrigomarques02@users.noreply.github.com>
2023-06-08 16:02:29 -0300

    Update main.py comiit 5
    Esse programa realiza uma pesquisa com 4 perguntas e grava as respostas em um arquivo CSV.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-
 # main.py
-# Gravação das respostas em um arquivo CSV
+# Gravação das respostas em um arquivo CSV (sem usar bibliotecas)
 
 perguntas = [
     "Você gosta de programar?",
@@ -15,10 +13,19 @@ for pergunta in perguntas:
     resposta = input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei): ")
     respostas.append(resposta)
 
+# Formatação das respostas como uma linha CSV
+linha_csv = ','.join(respostas) + '\n'
+
 # Gravação das respostas em um arquivo CSV
 nome_arquivo = "respostas.csv"
-with open(nome_arquivo, "a", newline="") as arquivo_csv:
-    escritor = csv.writer(arquivo_csv)
-    escritor.writerow(respostas)
+with open(nome_arquivo, "a") as arquivo_csv:
+    arquivo_csv.write(linha_csv)
 
 print("Respostas gravadas com sucesso no arquivo CSV.")
+
+# Documentação
+"""
+Esse programa realiza uma pesquisa com 4 perguntas e grava as respostas em um arquivo CSV.
+As perguntas são exibidas sequencialmente, e o usuário deve responder com 1 para Sim, 2 para Não, ou 3 para Não sei.
+As respostas são gravadas em um arquivo CSV chamado 'respostas.csv'.
+"""
