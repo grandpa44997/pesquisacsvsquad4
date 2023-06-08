bc869d385f3097fa7db9522bdc28d552cd7a99b1
Rodrigo Marques <132007834+Rodrigomarques02@users.noreply.github.com>
2023-06-08 15:56:56 -0300

    Update main.py comit2
    armazenando uma lista de perguntas em csv

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# main.py
-# Implementação do fluxo de perguntas e respostas
+import csv
 
-# Perguntas
+# main.py
+# Gravação das respostas em um arquivo CSV
+
 pergunta1 = "Você gosta de programar?"
 pergunta2 = "Você está estudando Python?"
 pergunta3 = "Você já trabalhou com análise de dados?"
 pergunta4 = "Você se sente confortável com estatísticas?"
 
-# Respostas
 respostas = []
 for pergunta in [pergunta1, pergunta2, pergunta3, pergunta4]:
-    resposta = input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei responder): ")
+    resposta = input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei): ")
     respostas.append(resposta)
 
-print("Respostas:", respostas)
+# Gravação das respostas em um arquivo CSV
+nome_arquivo = "respostas.csv"
+with open(nome_arquivo, "a", newline="") as arquivo_csv:
+    escritor = csv.writer(arquivo_csv)
+    escritor.writerow(respostas)
+
+print("Respostas gravadas com sucesso no arquivo CSV.")
