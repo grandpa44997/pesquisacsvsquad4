f6df51ae2d4cb505768119c5cbda74807c71afbf
Daniel Freitas <bikemessengerateu@gmail.com>
2023-06-07 22:11:13 -0300

    Update main.py estrutura de repetição
    estrutura de repetição para realizar pergunta e coleta de resposta em uma lista

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#main.py
+# main.py
+# Implementação do fluxo de perguntas e respostas
+
+# Perguntas
+pergunta1 = "Você gosta de programar?"
+pergunta2 = "Você está estudando Python?"
+pergunta3 = "Você já trabalhou com análise de dados?"
+pergunta4 = "Você se sente confortável com estatísticas?"
+
+# Respostas
+respostas = []
+for pergunta in [pergunta1, pergunta2, pergunta3, pergunta4]:
+    resposta = input(f"{pergunta} (1-Sim / 2-Não / 3-Não sei responder): ")
+    respostas.append(resposta)
+
+print("Respostas:", respostas)
